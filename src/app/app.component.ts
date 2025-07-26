1a0d0fc0dcdedb168af0cef326afb03e6191a792
Gayatri <takawalegayatri@gmail.com>
2025-07-26 17:44:08 +0530

    Add responsiveness

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { ToastComponent } from './components/toast/toast.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavigationComponent, ToastComponent],
+  imports: [RouterOutlet, NavigationComponent, ToastComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
